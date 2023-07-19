2ee4402a700abbf00cb0db3d9e415525710e1839
Juan Sierra <jsierrachaparro@gmail.com>
2023-07-18 23:07:12 -0500

    Se incorpora libreria readchar

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from readchar import readkey, key
+
 name = input ('Hello gamer, put your name or nickname: ') 
 
 print(f'{name}, welcome to this game !!!')
+
+
+while True:
+    k = readkey()
+    if k == key.UP:
+        break
+    else:
+        print(k)
+
